e69000d7ff2d5fe934eba52fe9f69d7f74521d4e
MattyJacques <iFtw@live.co.uk>
2016-08-27 22:45:53 +0100

    Changed ResourceManager to AnimalManager

---
+++ Assets/Scripts/Managers/AnimalManager.cs
@@ -0,0 +1,28 @@
+﻿// Title        : AnimalManager.cs
+// Purpose      : Initiates templates, manages instances of animals
+// Author       : Matthew Jacques
+// Date         : 27/08/2016
+
+using UnityEngine;
+using System.Collections;
+
+
+namespace ZooManager
+{
+
+  public class AnimalManager : MonoBehaviour
+  {
+
+    // Holds all animal templates read from JSON array
+    public AnimalTemplateCollection templates;
+
+    void Start()
+    { // Call to get the templates from JSON
+
+      templates = JSONReader.ReadJSON();
+
+    } // Start()
+
+  } // AnimalManager
+
+} // Namespace
