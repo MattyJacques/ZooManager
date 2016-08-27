8004b21bf965bcf995d7a4ab2379346c1b8fc49f
MattyJacques <iFtw@live.co.uk>
2016-08-27 23:33:44 +0100

    Added animal creation

--- Assets/Scripts/Managers/AnimalManager.cs
+++ Assets/Scripts/Managers/AnimalManager.cs
@@ -5,6 +5,7 @@
 
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic
 
 
 namespace ZooManager
@@ -13,8 +14,14 @@ namespace ZooManager
   public class AnimalManager : MonoBehaviour
   {
 
+    // Which mode to find the animal template with
+    enum CreateMode { ID, NAME };
+
     // Holds all animal templates read from JSON array
     public AnimalTemplateCollection templates;
+
+    List<AnimalBase> animals;
+
 
     void Start()
     { // Call to get the templates from JSON
@@ -22,6 +29,71 @@ namespace ZooManager
       templates = JSONReader.ReadJSON();
 
     } // Start()
+
+
+    public void CreateAnimal(int id)
+    { // Create an animal instance using the ID field of the templates
+
+      // Find index in array
+      int index = GetTemplateIndex(id, "", CreateMode.ID);
+
+      // Create new animal with found template
+      AnimalBase newAnimal = new AnimalBase(templates.animalTemplates[index]);
+
+      // Add animal to instances list
+      animals.Add(newAnimal);
+
+    } // CreateAnimal(id)
+
+
+    public void CreateAnimal(string name)
+    { // Create an animal instance using the name field of the templates
+
+      // Find index in array
+      int index = GetTemplateIndex(0, name, CreateMode.NAME);
+
+      // Create new animal with found template
+      AnimalBase newAnimal = new AnimalBase(templates.animalTemplates[index]);
+
+      // Add animal to instances list
+      animals.Add(newAnimal);
+
+    } // CreateAnimal(name)
+
+
+    private int GetTemplateIndex(int id, string name, CreateMode mode)
+    { // Get the template index using the name or id, whichever mode is passed in
+      // Returns -1 if not found
+
+      int templateIndex = -1;              // Holds the template index found
+
+      for (int i = 0; i < templates.animalTemplates.Length; i++)
+      { // Check if there is a match for every template in the array
+
+        if (mode == CreateMode.ID)
+        { // If mode is ID, check for matching ID
+
+          if (templates.animalTemplates[i].id == id)
+          { // Check for matching ID, if found set index and break out of loop
+            templateIndex = i;
+            break;
+          }
+        }
+        else if (mode == CreateMode.NAME)
+        { // If mode is name, check for matching name
+
+          if (templates.animalTemplates[i].animalname == name)
+          { // Check for matching name, if found set index and break out of loop
+            templateIndex = i;
+            break;
+          }
+        }
+      }
+
+      return templateIndex;
+
+    } // GetTemplateIndex()
+
 
   } // AnimalManager
 
