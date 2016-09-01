b652b9daf4d57a153735b2ae53a784ce330c0fe9
MattyJacques <iFtw@live.co.uk>
2016-09-01 21:20:55 +0100

    Added BuildingManager, changed console indentation (can be undone)

---
+++ Assets/Scripts/Managers/BuildingManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Managers
+{
+  public class BuildingManager : MonoBehaviour
+  {
+    private GameObject[] buildings;
+    private Assets.Scripts.UI.Console console;
+
+    void Start()
+    { // Initialise the game console
+      console = GetComponent<Assets.Scripts.UI.Console>();
+    } // Start()
+
+
+    void Create()
+
+  } // BuildingManager
+}
