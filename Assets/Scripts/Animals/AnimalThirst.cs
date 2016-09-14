9e9156fca0a16bffd9af4c19ecd56b668001193a
geotsak <geotsakvideo@gmail.com>
2016-09-14 22:25:24 +0300

    Made each animal behaviour have a seperate class.

---
+++ Assets/Scripts/Animals/AnimalThirst.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimalThirst {
+    public float thirst;
+    public Thirst needsWater;
+
+	// Use this for initialization
+    void Start () 
+    {
+        thirst = 100;
+    } // Start()
+	
+	// Update is called once per frame
+	void Update () {
+        if (thirst > 0) 
+        {
+            thirst--;
+        }
+
+        if (thirst <= 50)
+        {
+            needsWater = Thirst.True;
+        }
+        else 
+        {
+            needsWater = Thirst.False;
+        }
+        if (thirst == 0)
+        {
+            // Call Die Function
+        }
+  } // Update()
+} // AnimalThirst
+
+public enum Thirst 
+{
+    False,
+    True,
+    Drink
+} // Thirst
