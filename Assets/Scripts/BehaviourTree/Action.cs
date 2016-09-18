b755c81a6b4e517db7d471864e58bd21128eb0ae
MattyJacques <iFtw@live.co.uk>
2016-09-18 19:28:13 +0100

    Finished action node

--- Assets/Scripts/BehaviourTree/Action.cs
+++ Assets/Scripts/BehaviourTree/Action.cs
@@ -1,15 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class Action : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+namespace Assets.Scripts.BehaviourTree
+{
+  public delegate ReturnCode ActionDelegate();   // The action of the behaviour
+
+  public class Action : BehaveComponent
+  {
+    private ActionDelegate _action;
+
+    private Action() { }
+
+    public Action(ActionDelegate action)
+    { // Constructor to set up the action of the node
+      _action = action;
+    } // Action()
+
+
+    public override ReturnCode Behave()
+    {
+      try
+      {
+        switch (_action())
+        {
+          case ReturnCode.Success:
+            _returnCode = ReturnCode.Success;
+            return _returnCode;
+          case ReturnCode.Failure:
+            _returnCode = ReturnCode.Failure;
+            return _returnCode;
+          case ReturnCode.Running:
+            _returnCode = ReturnCode.Running;
+            return _returnCode;
+          default:
+            _returnCode = ReturnCode.Failure;
+            return _returnCode;
+        }
+      } // try
+      catch (Exception excep)
+      { // Print out the exception for debugging purposes and continue with
+        // behaviour checking
+
+        Debug.Log(excep.ToString());
+        _returnCode = ReturnCode.Failure;
+        return _returnCode;
+      } // catch
+    } // Behave()
+
+  } // Action()
 }
