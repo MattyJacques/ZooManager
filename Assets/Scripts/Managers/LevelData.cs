f8632fe7cb3dc524085f27290eeae821cd986236
geotsak <geotsakvideo@gmail.com>
2016-09-18 11:44:12 +0300

    Added LevelData.cs and LevelData.json

---
+++ Assets/Scripts/Managers/LevelData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Animals;
+using Assets.Scripts.Helpers;
+
+
+namespace Assets.Scripts.Managers
+{
+
+public class LevelData : MonoBehaviour {
+
+
+  } // LevelData
+} //Namespace
