e3a843b227b2d29d7fe53790dfd03193324e4006
geotsak <geotsakvideo@gmail.com>
2016-09-18 22:40:34 +0300

    Fixed 2 curly braces rip.

--- Assets/Scripts/Managers/LevelData.cs
+++ Assets/Scripts/Managers/LevelData.cs
@@ -19,10 +19,12 @@ public class LevelData : MonoBehaviour {
         { // Call to get the templates from JSON
 
             _templates = JSONReader.ReadJSON<LevelTemplateCollection>("LevelData");
-			foreach (LevelBuildingTemplate building in _templates.levels[0].buildings) {
+			foreach (LevelBuildingTemplate building in _templates.levels[0].buildings) 
+			{
 				GameObject obj = Instantiate(Resources.Load("Buildings/Prefabs/" + building.name, typeof(GameObject)), new Vector3(building.posX,building.posY,building.posZ), new Quaternion (building.rotX, building.rotY, building.rotZ, building.rotW)) as GameObject;
 			}
-			foreach (LevelAnimalTemplate animal in _templates.levels[0].animals) {
+			foreach (LevelAnimalTemplate animal in _templates.levels[0].animals) 
+			{
 				GameObject obj = Instantiate(Resources.Load("Animals/Prefabs/" + animal.name, typeof(GameObject)), new Vector3(animal.posX,animal.posY,animal.posZ), new Quaternion(0,0,0,0)) as GameObject;
 			} 
         } // Start()
