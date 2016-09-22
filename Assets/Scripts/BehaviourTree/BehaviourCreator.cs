1416af92741aad1deb61eb4d6abc3f92ead74b81
MattyJacques <iFtw@live.co.uk>
2016-09-22 21:10:55 +0100

    cleanup after r2

--- Assets/Scripts/BehaviourTree/BehaviourCreator.cs
+++ Assets/Scripts/BehaviourTree/BehaviourCreator.cs
@@ -11,13 +11,11 @@ namespace Assets.Scripts.BehaviourTree
   {
     private List<Base.Behaviour> _behaviours;
 
-
     public Base.Behaviour GetBehaviour(int index)
     { // Return the behaviour at the given index
 
       return _behaviours[index];
     } // GetBehaviour()
-
 
     public void CreateBehaviours()
     { // Create all of the behaviours needed, storing them in the list
@@ -25,12 +23,11 @@ namespace Assets.Scripts.BehaviourTree
       _behaviours = new List<Base.Behaviour>();
 
       BehaveComponent[] animalComponents = new BehaveComponent[1];
-      animalComponents[0] = CreateAnimalThirst2();
+      animalComponents[0] = CreateAnimalThirst();
       Selector animalSelector = new Selector(animalComponents);
       _behaviours.Add(new Base.Behaviour(animalSelector));
 
     } // CreateBehaviours()
-
 
     private Sequence CreateAnimalThirst()
     { // Creates a sequence that will allow an animal object to check
@@ -42,31 +39,8 @@ namespace Assets.Scripts.BehaviourTree
       BehaveComponent[] thirstCheckComp = new BehaveComponent[2];
 
       // Set FindWater components
-      findWaterComp[0] = new SetTarget();       // Find nearest water source
-      findWaterComp[1] = CreateMoveToTarget();
-      findWaterComp[2] = new Drink();               // Refill thirst
-
-      // Set thirst components
-      thirstCheckComp[0] = new IsThirsty();
-      thirstCheckComp[1] = new Sequence(findWaterComp);
-
-      // Create and return the finished thirst sequence
-      return new Sequence(thirstCheckComp);
-
-    } // CreateThirst()
-
-    private Sequence CreateAnimalThirst2()
-    { // Creates a sequence that will allow an animal object to check
-      // if thirsty, if so will find the nearest trough object and go
-      // drink
-
-      // Create Arrays to hold sequence components
-      BehaveComponent[] findWaterComp = new BehaveComponent[3];
-      BehaveComponent[] thirstCheckComp = new BehaveComponent[2];
-
-      // Set FindWater components
       findWaterComp[0] = new Action(SetCurrTarget);       // Find nearest water source
-      findWaterComp[1] = CreateMoveToTarget2();
+      findWaterComp[1] = CreateMoveToTarget();
       findWaterComp[2] = new Action(DrinkWater);               // Refill thirst
 
       // Set thirst components
@@ -79,25 +53,6 @@ namespace Assets.Scripts.BehaviourTree
     } // CreateThirst()
 
     private Sequence CreateMoveToTarget()
-    { // Create the sequence of behaviours that will allow an object to
-      // follow to path to a target
-
-      BehaveComponent[] moveToTargetComp = new BehaveComponent[4];
-      BehaveComponent[] pathCheckComp = new BehaveComponent[2];
-
-      pathCheckComp[0] = new HasPath();
-      pathCheckComp[1] = new GetPath();
-
-      moveToTargetComp[0] = new HasTarget();
-      moveToTargetComp[1] = new Selector(pathCheckComp);
-      moveToTargetComp[2] = new FollowPath();
-      moveToTargetComp[3] = new HasArrived();
-
-      return new Sequence(moveToTargetComp);
-      
-    } // CreateMoveToTarget()
-
-    private Sequence CreateMoveToTarget2()
     { // Create the sequence of behaviours that will allow an object to
       // follow to path to a target
 
