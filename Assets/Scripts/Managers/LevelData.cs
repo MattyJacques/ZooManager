ca197aff2d3bc4db3d69c7d6f26be29683dbfbf8
MattyJacques <iFtw@live.co.uk>
2016-09-29 22:39:43 +0100

    Tidied up LevelData, added Create() in BuildingManager

--- Assets/Scripts/Managers/LevelData.cs
+++ Assets/Scripts/Managers/LevelData.cs
@@ -8,28 +8,32 @@ using Assets.Scripts.Helpers;
 namespace Assets.Scripts.Managers
 {
 
-public class LevelData
-{
-        // Which mode to find the animal template with
-        enum CreateMode { ID, NAME };
-        public AnimalManager manager;
+  public class LevelData
+  {
+    public static LevelTemplateCollection _templates;
 
-        public LevelTemplateCollection _templates;
+    static void LoadLevelData()
+    { // Load the level data from LevelData.json, storing in _templates
 
-        void Start()
-        { // Call to get the templates from JSON
+      _templates = JSONReader.ReadJSON<LevelTemplateCollection>("LevelData");
 
-            _templates = JSONReader.ReadJSON<LevelTemplateCollection>("LevelData");
-			foreach (LevelBuildingTemplate building in _templates.levels[0].buildings) 
-			{
-				GameObject obj = Instantiate(Resources.Load("Buildings/Prefabs/" + building.name, typeof(GameObject)), new Vector3(building.posX,building.posY,building.posZ), new Quaternion (building.rotX, building.rotY, building.rotZ, building.rotW)) as GameObject;
-			}
-			foreach (LevelAnimalTemplate animal in _templates.levels[0].animals) 
-			{
-				GameObject obj = Instantiate(Resources.Load("Animals/Prefabs/" + animal.name, typeof(GameObject)), new Vector3(animal.posX,animal.posY,animal.posZ), new Quaternion(0,0,0,0)) as GameObject;
-			} 
-        } // Start()
-  
+    } // LoadLevelData()
+
+    void LoadLevel(int index)
+    { // Load the level that corrosponds to the index given as a argument
+
+      foreach (LevelBuildingTemplate building in _templates.levels[index].buildings)
+      {
+
+      }
+
+      foreach (LevelBuildingTemplate building in _templates.levels[index].buildings)
+      {
+
+      }
+
+    } // LoadLevel()
 
   } // LevelData
+
 } //Namespace
