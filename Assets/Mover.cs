eca5641d1bccdea17fabbb6316f560b10bf4664d
Amberite <jakekennard.tj@gmail.com>
2016-11-30 19:13:46 +0000

    Revert "YARP"
    This reverts commit 1f9c582258ce26c8eaced022098ff3d83b40a1bf.

---
+++ Assets/Mover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+    Transform target;
+    Seeker seeker;
+
+	void Start () {
+
+        seeker = GetComponent<Seeker>();
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+	}
+	
+    public void OnPathComplete (Pathfinding // here, pos 22:29 in vid
+        
+}
