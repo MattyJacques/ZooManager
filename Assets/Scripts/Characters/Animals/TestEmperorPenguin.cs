9de7f0cdc27489ed7d7376e3d5aefb4f3bc7beac
Chii <renkinju98@gmail.com>
2017-01-26 15:43:09 -0500

    refactored +improvements to wander logic

--- Assets/Scripts/Characters/Animals/TestEmperorPenguin.cs
+++ Assets/Scripts/Characters/Animals/TestEmperorPenguin.cs
@@ -7,44 +7,50 @@ public class TestEmperorPenguin : MonoBehaviour{
   private Animator anim;
   private Vector2 wayPoint;
   private Rigidbody rbody;
-
-  float x;
+  private int radius=47;
+  private float minWait=0;
+  private float maxWait=5;
 
   // Use this for initialization
 	void Start () 
   {
-      anim = GetComponent<Animator>();
-      rbody = GetComponent<Rigidbody>();
-      
-      //TODO, move to Update() later...
-      InvokeRepeating("Wander", 2, 20);
+    anim = GetComponent<Animator>();
+    rbody = GetComponent<Rigidbody>();
+
+    StartCoroutine(ChooseDestination());
 	}
 
-  void Wander() 
-  { 
-    //random point to wander to, 47 is the radius
-    wayPoint = Random.insideUnitCircle *47;
+  IEnumerator ChooseDestination()
+  {
+    while(true)
+    {
+      wayPoint = Random.insideUnitCircle * radius;
+      float moveX = wayPoint.x * Time.deltaTime;
+      float moveZ = wayPoint.y * Time.deltaTime;
 
+      Wander(moveX, moveZ);
+      yield return new WaitForSeconds(Random.Range(minWait, maxWait));
+    }
+  }
+
+  void Wander(float x, float z) 
+  {    
     //TODO: blend animations
-    //anim.SetFloat("PosX", wayPoint.x);
-    //anim.SetFloat("PosY", wayPoint.y);
+    //anim.SetFloat("PosX", x);
+    //anim.SetFloat("PosY", z);
 
-    //sets movement point
-    float moveX = wayPoint.x*Time.deltaTime;
-    float moveZ = wayPoint.y*Time.deltaTime;
+    rbody.velocity = new Vector3(x, 0, z).normalized;
+    //TODO: replace with above with below but range would be size of enclosure
+    //rbody.velocity = new Vector3(Random.Range(-x, x), 0, Random.Range(-z, z).normalized;
 
-    //TODO: replace with pathfinding
-    rbody.velocity = new Vector3(moveX, 0, moveZ).normalized;
-
-    //changed rotation to -velocity because GameObject was moonwalking
-    Quaternion DesiredRotation = Quaternion.LookRotation(-rbody.velocity);
-    transform.rotation = DesiredRotation;
+    Quaternion targetRotation = Quaternion.LookRotation(-rbody.velocity);
+    transform.rotation = targetRotation;
 
     anim.Play("Walk", -1);
 
-    Debug.Log("Waypoint=" + wayPoint + "Velocity=" + rbody.velocity.magnitude + "Rotation=" + rbody.rotation); 
+    Debug.Log("Waypoint=" + wayPoint + "Velocity=" + rbody.velocity.magnitude + "Rotation=" + rbody.rotation);
   }
-	
+
   // Update is called once per frame
   void Update()
   { // Process the needs of the base then process the behaviour for AI
