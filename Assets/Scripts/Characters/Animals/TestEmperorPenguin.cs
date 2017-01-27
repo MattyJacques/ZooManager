0b91f3af20d4be47ef987f44b470715258141686
Chii <renkinju98@gmail.com>
2017-01-26 19:18:36 -0500

    Additional changes to penguin animation controller

--- Assets/Scripts/Characters/Animals/TestEmperorPenguin.cs
+++ Assets/Scripts/Characters/Animals/TestEmperorPenguin.cs
@@ -8,53 +8,53 @@ public class TestEmperorPenguin : MonoBehaviour{
   private Vector2 wayPoint;
   private Rigidbody rbody;
   private int radius=47;
-  private float minWait=0;
-  private float maxWait=5;
+  private float minWalk = 2;
+  private float maxWalk = 5;
+  private float minWait = 3;
+  private float maxWait = 5;
 
   // Use this for initialization
 	void Start () 
   {
     anim = GetComponent<Animator>();
     rbody = GetComponent<Rigidbody>();
-
-    StartCoroutine(ChooseDestination());
+    WalkorIdle();
 	}
 
-  IEnumerator ChooseDestination()
+  void WalkorIdle()
   {
-    while(true)
-    {
-      wayPoint = Random.insideUnitCircle * radius;
-      float moveX = wayPoint.x * Time.deltaTime;
-      float moveZ = wayPoint.y * Time.deltaTime;
-
-      Wander(moveX, moveZ);
-      yield return new WaitForSeconds(Random.Range(minWait, maxWait));
-    }
+    //50% chance to idle when destination reached
+    float randomFloat = Random.Range(0f, 1f); 
+    if (randomFloat < 0.5f)
+      StartCoroutine(Idle());
+    else 
+      StartCoroutine(Wander());
   }
 
-  void Wander(float x, float z) 
-  {    
-    //TODO: blend animations
-    //anim.SetFloat("PosX", x);
-    //anim.SetFloat("PosY", z);
-
-    rbody.velocity = new Vector3(x, 0, z).normalized;
-    //TODO: replace with above with below but range would be size of enclosure
-    //rbody.velocity = new Vector3(Random.Range(-x, x), 0, Random.Range(-z, z).normalized;
-
+  IEnumerator Wander() 
+  {
+    float moveX = 10;
+    float moveZ = 10;
+    //TODO: replace below with range of enclosure
+    Vector3 waypoint = new Vector3(Random.Range(-moveX, moveX), 0, Random.Range(-moveZ, moveZ));
+    rbody.velocity = waypoint.normalized;
     Quaternion targetRotation = Quaternion.LookRotation(-rbody.velocity);
     transform.rotation = targetRotation;
+    yield return new WaitForSeconds(Random.Range(minWalk, maxWalk));
+    WalkorIdle();
+  }
 
-    anim.Play("Walk", -1);
-
-    Debug.Log("Waypoint=" + wayPoint + "Velocity=" + rbody.velocity.magnitude + "Rotation=" + rbody.rotation);
+  IEnumerator Idle()
+  {
+    rbody.velocity = new Vector3(0, 0, 0);
+    yield return new WaitForSeconds(Random.Range(minWait, maxWait));
+    WalkorIdle();
   }
 
   // Update is called once per frame
   void Update()
   { // Process the needs of the base then process the behaviour for AI
-    
+    anim.SetFloat("speed", rbody.velocity.magnitude);
     /*
     if(****ISHUNGRY******){
      //TODO: move to food
