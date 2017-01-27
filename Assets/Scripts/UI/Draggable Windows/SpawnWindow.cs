693f0769b8927718a442f47541098769c46c1704
aimmmmmmmmm <ovo414@sbcglobal.net>
2017-01-27 15:46:41 -0600

    Added Building Window + Animal Window. Still waiting on Buildings.json to link it up! Also, fixed a bug in Animals.json

---
+++ Assets/Scripts/UI/Draggable Windows/SpawnWindow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWindow : MonoBehaviour {
+  public GameObject windowPrefab;
+  private GameObject window;
+  public void OnClick()
+  {
+    if (window == null) { window = (GameObject)Instantiate (windowPrefab, new Vector3 (0, 0, 0), Quaternion.identity); } 
+    else { Destroy (window); }
+  }
+}
