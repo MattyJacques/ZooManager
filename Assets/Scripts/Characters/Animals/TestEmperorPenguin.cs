dbadb94cf3e2672742d851635c7c3191ab5ccf41
Chii <renkinju98@gmail.com>
2017-02-08 21:16:42 -0500

    more lines of comments than there are meaningful code

--- Assets/Scripts/Characters/Animals/TestEmperorPenguin.cs
+++ Assets/Scripts/Characters/Animals/TestEmperorPenguin.cs
@@ -9,15 +9,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TestEmperorPenguin : MonoBehaviour{
-  private Animator anim;
-  private Vector2 wayPoint;
-  private Rigidbody rbody;
-  private int radius=47;
-  private float minWalk = 2;
-  private float maxWalk = 5;
-  private float minWait = 3;
-  private float maxWait = 5;
-  private float walkSpeed, rotationSpeed;
+  private Animator anim;  //for accessing the GameObject's animator
+  private Rigidbody rbody;  //for accessing the GameObject's rigidbidy
+  private int radius=47;  //radius of the circle the animals can wander...this will be removed and replaced
+  private float minWalk = 2;  //minimum amount of time taken walking
+  private float maxWalk = 5;  //maximum amount of time taken walking
+  private float minWait = 3;  //minimum amount of time taken idling
+  private float maxWait = 5;  //maximum amount of time taken idling
+  private float walkSpeed, rotationSpeed; //walking speed and rotation speed
 
   // Use this for initialization
 	void Start () 
@@ -30,8 +29,7 @@ public class TestEmperorPenguin : MonoBehaviour{
 	}//Start()
 
   void WalkorIdle()
-  {
-    //50% chance to idle when destination reached
+  {//50% chance to idle when destination reached
     float randomFloat = Random.Range(0f, 1f);
     if (randomFloat < 0.5f)
     {
@@ -44,7 +42,7 @@ public class TestEmperorPenguin : MonoBehaviour{
   }//WalkorIdle()
 
   IEnumerator Wander() 
-  {
+  {//picks a random waypoint and has the animal walk towards it
     float moveX = 10;
     float moveZ = 10;
     //TODO: replace below with range of enclosure
@@ -56,7 +54,7 @@ public class TestEmperorPenguin : MonoBehaviour{
   }//Wander()
 
   IEnumerator Idle()
-  {
+  {//this makes the animal stand 
     rbody.velocity = Vector3.zero;
     yield return new WaitForSeconds(Random.Range(minWait, maxWait));
     WalkorIdle();
