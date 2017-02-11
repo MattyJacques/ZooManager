3735d827b05d37c7fc413f125632355c0a2c748c
Eivind A <deliusdahl@gmail.com>
2017-02-11 16:50:54 +0100

    Began fleshing out the skeleton for the VisitorManager class

--- Assets/Scripts/Managers/VisitorManager.cs
+++ Assets/Scripts/Managers/VisitorManager.cs
@@ -12,6 +12,56 @@ namespace Assets.Scripts.Managers
 {
   public class VisitorManager : MonoBehaviour
   {
+    //Notes:
+    //About visitor AI
+    //Call the StartBehaviour method after creation/placement, Call the StopBehaviour method before destroying
 
+    public List<Visitor> _activeVisitors = new List<Visitor> ();
+    private GameObject[] VisitorPrefabs;
+    //TODO: Object pool for destroying and creating new visitors, if it happens frequently enough
+
+    public class Visitor
+    {
+      public GameObject gameObject; //The instantiated in-game GameObject
+    }
+
+    private void Start()
+    {
+      LoadVisitorPrefabs ();
+    } //Start()
+
+    public void CreateVisitor(Vector3 position, GameObject visitorPrefab)
+    { //Creates a new visitor based on specific parameters
+
+      Visitor newVisitor = new Visitor ();
+      //TODO: Implement when ai is merged
+      //newVisitor.getAIScript.StartBehaviour();
+      _activeVisitors.Add (newVisitor);
+
+    } //CreateVisitor()
+
+    public void CreateRandomVisitor(Vector3 position)
+    { //Creates a random visitor
+
+      int random = Random.Range (0, VisitorPrefabs.Length);
+      CreateVisitor (position, VisitorPrefabs[random]);
+
+    } //CreateRandomVisitor()
+
+    public void DestroyVisitor(Visitor visitor)
+    { //Destroys a specific visitor
+
+      //TODO: implement when AI is merged
+      //visitor.getAIScript.StopBehaviour();
+      _activeVisitors.Remove (visitor);
+      //TODO: Destroy gameObject
+
+    } //DestroyVisitor()
+
+    private void LoadVisitorPrefabs()
+    { //Loads the prefab visitors from the visitor prefabs folder
+
+      VisitorPrefabs = Resources.LoadAll<GameObject> ("Visitors/Prefabs");
+    } //LoadVisitorPrefabs()
   }
 }
