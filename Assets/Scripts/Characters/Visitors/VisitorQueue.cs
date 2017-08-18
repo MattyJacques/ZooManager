960061c8acb8de93d218b944e8235e7b368b8c2f
Alex Falk <alexf12345@gmail.com>
2017-08-18 12:57:20 +0200

    Set up class for Visitor Queues
    This class does not do anything yet, but has the basic function names and a list of thing to do

---
+++ Assets/Scripts/Characters/Visitors/VisitorQueue.cs
@@ -0,0 +1,42 @@
+﻿// Title        : VehicleManager.cs
+// Purpose      : Creates, destroys, and manages visitors
+// Author       :  Alexander Falk
+// Date         : 11.02.2017
+//
+using System.Collections;
+using System.Collections.Generic;         // Lists
+using System.IO;                          // Directory Infos
+using UnityEngine;                        // Monobehaviour
+using Assets.Scripts.Characters.Vehicles;  // AnimalBAse
+using Assets.Scripts.BehaviourTree;       // Behaviours
+using Assets.Scripts.Helpers;             // JSONReader
+
+
+namespace Assets.Scripts.Characters.Visitors
+{
+  public class VisitorQueue : VisitorBase
+  {
+
+    public List<VisitorBase> Queue = new List<VisitorBase>();
+
+    public void CreateQueue()
+    {
+      //TODO: Find every TicketBooth
+      //TODO: Check if there's a queue line 
+      //TODO: If there is create spaces along the line for visitors to wait until a space in front opens
+      //TODO: If there isn't a queue line make visitors wait in a spot 
+    }
+
+    public void CheckPosition()
+    {
+      //TODO: Check if there is a person within the target 
+      //TODO: If there is put a visitor in a list of waiting visitors
+    }
+
+    public void UpdateQueue()
+    {
+      //TODO: As soon as the visitor in the target leaves, send the first one in the list to the target
+    }
+  }
+}
+
