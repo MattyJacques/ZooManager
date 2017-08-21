71a219b1183de7dbc0bdbf5a8ed218447364ba88
Alex Falk <alexf12345@gmail.com>
2017-08-21 21:24:27 +0200

    Implementing Some of the GUI functionality

---
+++ Assets/Scripts/UI/UIControl.cs
@@ -0,0 +1,90 @@
+﻿// Title        : UIControl
+// Purpose      : Lets the user control things using the UI
+// Author       : Alexander Falk
+// Date         : 21/08/2017
+
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections;
+
+public class UIControl : MonoBehaviour
+{
+  private RtsCamera cameramain;
+  private RtsCameraMouse cameramouse;
+  private RtsCameraKeys camerakeys;
+
+  private bool zoomIn;
+  private bool zoomOut;
+  private bool rotateLeft;
+  private bool rotateRight;
+
+  void Start()
+  { // Loads in the camera elements to be used
+    cameramain = GameObject.Find("Main Camera").GetComponent<RtsCamera>();
+    camerakeys = GameObject.Find("Main Camera").GetComponent<RtsCameraKeys>();
+
+  } // Start()
+
+  void Update()
+  {
+    CameraControls();
+  } // Update()
+
+  void CameraControls()
+  { // Camera Control Functions
+    if (zoomIn && camerakeys.AllowZoom)
+    {
+      cameramain.Distance -= camerakeys.ZoomSpeed * Time.deltaTime;
+    }
+
+    if (zoomOut && camerakeys.AllowZoom)
+    {
+      cameramain.Distance += camerakeys.ZoomSpeed * Time.deltaTime;
+    }
+
+    if (rotateLeft)
+    {
+      cameramain.Rotation += camerakeys.RotateSpeed * Time.deltaTime;
+    }
+
+    if (rotateRight)
+    {
+      cameramain.Rotation -= camerakeys.RotateSpeed * Time.deltaTime;
+    }
+  } // CameraControls()
+
+  public void ZoomIn()
+  {
+    zoomIn = true;
+  }
+
+  public void ZoomOut()
+  {
+    zoomOut = true;
+  }
+
+  public void RotateLeft()
+  {
+    rotateLeft = true;
+  }
+
+  public void RotateRight()
+  {
+    rotateRight = true;
+  }
+
+  public void Stop()
+  {
+    zoomIn = false;
+    zoomOut = false;
+    rotateLeft = false;
+    rotateRight = false;
+  }
+}
+
+
+
+
+
