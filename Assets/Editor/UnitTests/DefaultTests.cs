eec3d6b12566827806f4a3b41ec8bc7cbfdbcd4c
Jake Huxell <darkduckmovies@gmail.com>
2017-10-25 15:22:12 +0100

    Importing UnityTestTools, updating AStar and writing a test that should make the build fail.

---
+++ Assets/Editor/UnitTests/DefaultTests.cs
@@ -0,0 +1,18 @@
+﻿// Sifaka Game Studios (C) 2017
+
+using NUnit.Framework;
+
+namespace Assets.Editor.UnitTests
+{
+    [TestFixture]
+    public class DefaultTestFixture
+    {
+        [Test]
+        public void WhenTestFails_FailsBuild()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+
+}
+
