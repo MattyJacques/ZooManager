207b6661eeacca2fc9d678c6167009256b2ecaa2
Jake Huxell <darkduckmovies@gmail.com>
2017-10-27 20:56:04 +0100

    Actual test for BehaviourAction

---
+++ Assets/Scripts/Tests/BehaviourTree/Base/BehaviourActionTests.cs
@@ -0,0 +1,66 @@
+﻿// Sifaka Game Studios (C) 2017
+
+using System;
+using System.Collections;
+using Assets.Scripts.BehaviourTree.Base;
+using Assets.Scripts.Characters;
+using Assets.Scripts.Tests.Characters;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Assets.Scripts.Tests.BehaviourTree.Base
+{
+    [TestFixture]
+    public class BehaviourActionTestFixture
+    {
+        private AIBase _aiBase;
+        private static ReturnCode ExpectedReturnCode = ReturnCode.Running;
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            _aiBase = new AIBaseBuilder().Build();
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            _aiBase = null;
+        }
+
+        [UnityTest]
+        public IEnumerator ActionDelegateDoesNotAlterResult_ReturnsFailure()
+        {
+            var action = new BehaviourAction(new ActionDelegate(EmptyAction));
+
+            var returnCode = ReturnCode.Success;
+
+            yield return CoroutineSys.Instance.StartCoroutine(action.Behave(_aiBase, val => { returnCode = val; }));
+
+            Assert.AreEqual(ReturnCode.Failure, returnCode);
+        }
+
+        [UnityTest]
+        public IEnumerator ActionDelegateAltersResult_ReturnsAlteredResult()
+        {
+            var action = new BehaviourAction(new ActionDelegate(AlteringAction));
+
+            var returnCode = ReturnCode.Success;
+
+            yield return CoroutineSys.Instance.StartCoroutine(action.Behave(_aiBase, val => { returnCode = val; }));
+
+            Assert.AreEqual(ExpectedReturnCode, returnCode);
+        }
+
+        private static IEnumerator EmptyAction(AIBase inAiBase, Action<ReturnCode> returnCode)
+        {
+            yield return null;
+        }
+
+        private static IEnumerator AlteringAction(AIBase inAiBase, Action<ReturnCode> returnCode)
+        {
+            returnCode(ExpectedReturnCode);
+            yield return null;
+        }
+    }
+}
