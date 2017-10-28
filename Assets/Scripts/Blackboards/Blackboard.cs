030781060582c5f548c5fbbcfd5a881c6896a7e8
Jake Huxell <darkduckmovies@gmail.com>
2017-10-28 15:44:31 +0100

    Adding Blackboard for behaviour tree

---
+++ Assets/Scripts/Blackboards/Blackboard.cs
@@ -0,0 +1,12 @@
+﻿// Sifaka Game Studios (C) 2017
+
+using BlackboardContainer = System.Collections.Generic.Dictionary<string, Assets.Scripts.Blackboards.BlackboardItem>;
+
+namespace Assets.Scripts.Blackboards
+{
+    public class Blackboard
+    {
+        public static BlackboardContainer GlobalBlackboard { get; set; }
+        public BlackboardContainer InstanceBlackboard { get; set; }
+    }
+}
