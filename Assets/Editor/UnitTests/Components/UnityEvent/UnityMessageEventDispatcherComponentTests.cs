508cc810f9631125c9945839023360f71b94e534
Jake Huxell <darkduckmovies@gmail.com>
2017-10-31 17:51:24 +0000

    Adding generic cross-component dispatcher

---
+++ Assets/Editor/UnitTests/Components/UnityEvent/UnityMessageEventDispatcherComponentTests.cs
@@ -0,0 +1,29 @@
+﻿// Sifaka Game Studios (C) 2017
+
+#if UNITY_EDITOR
+
+using Assets.Scripts.Tests.Components.UnityEvent;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Assets.Editor.UnitTests.Components.UnityEvent
+{
+    [TestFixture]
+    public class UnityMessageEventDispatcherComponentTestFixture {
+
+        [Test]
+        public void Awake_GetMessageEventDispatcher_ReturnsValidDispatcher()
+        {
+            var gameObject = new GameObject();
+
+            var unityMessageEventDispatcherComponent =
+                gameObject.AddComponent<TestUnityMessageEventDispatcherComponent>();
+
+            unityMessageEventDispatcherComponent.TestAwake();
+
+            Assert.NotNull(unityMessageEventDispatcherComponent.GetUnityMessageEventDispatcher());
+        }
+    }
+}
+
+#endif
---
+++ Assets/Scripts/Components/UnityEvent/UnityMessageEventHandle.cs
@@ -0,0 +1,44 @@
+﻿// Sifaka Game Studios (C) 2017
+
+using UnityEngine.Events;
+
+namespace Assets.Scripts.Components.UnityEvent
+{
+    public class UnityMessageEventHandle<TMessageType>
+        where TMessageType : UnityMessagePayload
+    {
+        public UnityMessageEventHandle()
+        {
+            _action = null;
+        }
+
+        public void RegisterHandle(UnityMessageEvent<TMessageType> inEvent, UnityAction<TMessageType> inAction)
+        {
+            if (inAction == null || inEvent == null)
+            {
+                throw new UnityMessageHandleException("Invalid action or event on registration!");
+            }
+
+            _action = inAction;
+            inEvent.AddListener(inAction);
+        }
+
+        public void UnregisterHandle(UnityMessageEvent<TMessageType> inEvent)
+        {
+            if (inEvent == null)
+            {
+                throw new UnityMessageHandleException("Invalid event on unregistration!");
+            }
+
+            inEvent.RemoveListener(_action);
+            _action = null;
+        }
+
+        public bool IsRegistered()
+        {
+            return _action != null;
+        }
+
+        private UnityAction<TMessageType> _action;
+    }
+}
