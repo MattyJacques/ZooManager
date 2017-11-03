5c6347344785a656bb06e5f215c35442ac6df339
Jake Huxell <darkduckmovies@gmail.com>
2017-11-03 14:59:06 +0000

    Adding basic concept of a "Need"

---
+++ Assets/Editor/UnitTests/Components/Needs/NeedTests.cs
@@ -0,0 +1,41 @@
+﻿// Sifaka Game Studios (C) 2017
+
+using Assets.Scripts.Components.Needs;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Assets.Editor.UnitTests.Components.Needs
+{
+    [TestFixture]
+    public class NeedTestFixture
+    {
+        [Test]
+        public void AdjustNeed_UpdatesCurveValueCorrectly()
+        {
+            const int maxHealth = 100;
+            const int adjustmentValue = -50;
+            const int expectedHealthChange = -10;
+
+            // Create curve with differing points to make sure it's not a simple straight line graph
+            var curve = new AnimationCurve();
+            curve.AddKey((((maxHealth + adjustmentValue)/maxHealth) * 100 ), expectedHealthChange);
+            curve.AddKey(100, expectedHealthChange + 100);
+            curve.AddKey(0, expectedHealthChange - 100);
+
+            var need = new Need(maxHealth, 11f, curve);
+
+            need.AdjustNeed(adjustmentValue);
+            Assert.AreEqual(expectedHealthChange, need.GetHealthAdjustment());
+        }
+
+        [Test]
+        public void GetUpdateFrequency_ReturnsAssignedUpdateFrequency()
+        {
+            const float expectedUpdateFrequency = 100f;
+            
+            var need = new Need(1, expectedUpdateFrequency, new AnimationCurve());
+
+            Assert.AreEqual(expectedUpdateFrequency, need.GetUpdateFrequency());
+        }
+    }
+}
---
+++ Assets/Scripts/Components/Needs/Need.cs
@@ -0,0 +1,40 @@
+﻿// Sifaka Game Studios (C) 2017
+
+using UnityEngine;
+
+namespace Assets.Scripts.Components.Needs
+{
+    public class Need
+    {
+        private readonly int _maxValue;
+        private readonly float _updateFrequency;
+        private readonly AnimationCurve _healthAdjustmentCurve;
+
+        private int _currentValue;
+
+        public Need(int inMaxValue, float inUpdateFrequency, AnimationCurve inHealthAdjustmentCurve)
+        {
+            _maxValue = inMaxValue;
+            _currentValue = inMaxValue;
+
+            _updateFrequency = inUpdateFrequency;
+
+            _healthAdjustmentCurve = inHealthAdjustmentCurve;
+        }
+
+        public void AdjustNeed(int inAdjustment)
+        {
+            _currentValue = Mathf.Clamp(_currentValue + inAdjustment, 0, _maxValue);
+        }
+
+        public int GetHealthAdjustment()
+        {
+            return (int)_healthAdjustmentCurve.Evaluate((_currentValue / _maxValue) * 100);
+        }
+
+        public float GetUpdateFrequency()
+        {
+            return _updateFrequency;
+        }
+    }
+}
