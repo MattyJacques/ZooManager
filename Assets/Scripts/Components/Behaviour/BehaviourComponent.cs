c049202210d7d67f3317a3b36f5e5ca5b181829d
Jake Huxell <darkduckmovies@gmail.com>
2017-11-11 14:31:06 +0000

    BehaviourComponent to run the tree

---
+++ Assets/Scripts/Components/Behaviour/BehaviourComponent.cs
@@ -0,0 +1,26 @@
+﻿// Sifaka Game Studios (C) 2017
+
+using Assets.Scripts.Behaviours;
+using UnityEngine;
+
+namespace Assets.Scripts.Components.Behaviour
+{
+    public class BehaviourComponent 
+        : MonoBehaviour
+    {
+        public string BehaviourName;
+
+        protected void Start ()
+        {
+            var behaviour = BehaviourCreator.Instance.GetBehaviour(BehaviourName);
+            if (behaviour != null)
+            {
+                StartCoroutine(behaviour.Behave(gameObject));
+            }
+            else
+            {
+                Debug.LogError("Tried to load behaviour with name " + BehaviourName + " which does not exist!");
+            }
+        }
+    }
+}
