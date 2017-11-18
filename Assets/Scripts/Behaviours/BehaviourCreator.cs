920bc5e17ffb184ca61b83fb27c1362a1676dee1
Jake Huxell <darkduckmovies@gmail.com>
2017-11-18 22:21:19 +0000

    VisitorBehaviourTree added.

--- Assets/Scripts/Behaviours/BehaviourCreator.cs
+++ Assets/Scripts/Behaviours/BehaviourCreator.cs
@@ -3,6 +3,7 @@
 using System.Collections.Generic;
 using Assets.Scripts.Behaviours.Animal;
 using Assets.Scripts.Behaviours.Base;
+using Assets.Scripts.Behaviours.General;
 using UnityEngine;
 
 namespace Assets.Scripts.Behaviours
@@ -43,20 +44,36 @@ namespace Assets.Scripts.Behaviours
         private void CreateBehaviours()
         { // Create all of the behaviours needed, storing them in the list
 
-            _behaviours = new Dictionary<string, BehaviourTree>();
+            _behaviours = new Dictionary<string, BehaviourTree>
+            {
+                {"basicAnimal", CreateBasicAnimalTree()},
+                {"basicVisitor", CreateBasicVisitorTree()}
+            };
 
-            var tree = new BehaviourTreeBuilder()
-                      .AddSelector()
-                          .AddSequence()
-                              .AddConditional(AnimalBehaviours.TryFindNeedToImprove)
-                              .AddAction(AnimalBehaviours.MoveToTarget)
-                              .AddAction(AnimalBehaviours.ImproveNeed)
-                          .AddSequence()
-                              .AddAction(AnimalBehaviours.GetRandomInterestPoint)
-                              .AddAction(AnimalBehaviours.MoveToTarget)
-                      .Build();
+        }
 
-            _behaviours.Add("basicAnimal", tree);
+        private static BehaviourTree CreateBasicAnimalTree()
+        {
+            return new BehaviourTreeBuilder()
+                .AddSelector()
+                    .AddSequence()
+                        .AddConditional(AnimalBehaviours.TryFindNeedToImprove)
+                        .AddAction(PathfindingBehaviours.MoveToTarget)
+                        .AddAction(AnimalBehaviours.ImproveNeed)
+                    .AddSequence()
+                        .AddAction(PathfindingBehaviours.GetRandomPointInsideEnclosure)
+                        .AddAction(PathfindingBehaviours.MoveToTarget)
+                .Build();
+        }
+
+        private static BehaviourTree CreateBasicVisitorTree()
+        {
+            return new BehaviourTreeBuilder()
+                .AddSelector()
+                    .AddSequence()
+                        .AddAction(PathfindingBehaviours.GetRandomInterestPoint)
+                        .AddAction(PathfindingBehaviours.MoveToTarget)
+                .Build();
         }
     } 
 }
